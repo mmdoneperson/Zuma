7c97cfb905491ebe59ef4a498362219e32cb39d6
mmdoneperson <mohmaks220@gmail.com>
2024-08-24 16:34:37 +0500

    что-то думаем 2

--- frog.py
+++ frog.py
@@ -6,11 +6,10 @@ import math
 class Frog:
     def __init__(self, screen):
         self.screen = screen
-        self.sprite_image = pg.transform.scale(pg.image.load("player.png"),(120, 120))
+        self.sprite_image = pg.transform.scale(pg.image.load("player.png"), (120, 120))
         self.center = Vector2(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
         self.rect = self.sprite_image.get_rect(center=self.center)
         self.coord = Vector2(0, 1)
-
 
     def update(self):
         x, y = pg.mouse.get_pos()
@@ -20,8 +19,7 @@ class Frog:
         sprite_rect = rotated_sprite.get_rect(center=self.rect.center)
         self.screen.blit(rotated_sprite, sprite_rect)
         self.coord = vect
-        #pg.draw.rect(self.screen, [0, 255, 0], [225, 225, 25, 25])
-
+        # pg.draw.rect(self.screen, [0, 255, 0], [225, 225, 25, 25])
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
